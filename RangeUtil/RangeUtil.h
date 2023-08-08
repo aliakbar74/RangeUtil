eef811e3532e3b7333dde253be5ba1e2e7304654
al.maleki <mraliakbarmaleki@gmail.com>
2023-08-08 17:45:16 +0330

    Add return overload for GetRange and GetRAnge_w

--- RangeUtil/RangeUtil.h
+++ RangeUtil/RangeUtil.h
@@ -10,10 +10,27 @@ extern "C" {
 }
 #endif
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ __declspec(dllexport) float GetRange(const RangeValue range, const float seed);
+#ifdef __cplusplus
+}
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
  __declspec(dllexport) void GetRange_W(const WeightedRangeValue * values, const unsigned int size, const unsigned int seed, float& result);
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ __declspec(dllexport) float GetRange_W(const WeightedRangeValue * values, const unsigned int size, const unsigned int seed);
 #ifdef __cplusplus
 }
 #endif
--- RangeUtil/Util.cpp
+++ RangeUtil/Util.cpp
@@ -8,6 +8,13 @@ void GetRange(const RangeValue range, const float seed, float& result)
 	result = dis(gen);
 }
 
+float GetRange(const RangeValue range, const float seed)
+{
+	std::mt19937 gen(seed);
+	std::uniform_real_distribution<float> dis(range.min, range.max);
+	return dis(gen);
+}
+
 void GetRange_W(const WeightedRangeValue* values, const unsigned int size, const unsigned int seed, float& result)
 {
 	int totalWeight = 0;
@@ -33,4 +40,30 @@ void GetRange_W(const WeightedRangeValue* values, const unsigned int size, const
 
 	std::uniform_real_distribution<float> floatDis(values[selectedId].min, values[selectedId].max);
 	result = floatDis(gen);
+}
+float GetRange_W(const WeightedRangeValue* values, const unsigned int size, const unsigned int seed)
+{
+	int totalWeight = 0;
+
+	for (auto i = 0; i < size; i++)
+	{
+		totalWeight += values[i].weight;
+	}
+
+	std::mt19937 gen(seed);
+	std::uniform_int_distribution<int> dis(0, totalWeight);
+	int rndWeight = dis(gen);
+	int selectedId = 0;
+	int passedWeight = 0;
+	for (auto i = 0; i < size; i++)
+	{
+		passedWeight += values[i].weight;
+		if (rndWeight <= passedWeight) {
+			selectedId = i;
+			break;
+		}
+	}
+
+	std::uniform_real_distribution<float> floatDis(values[selectedId].min, values[selectedId].max);
+	return floatDis(gen);
 }
